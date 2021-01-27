c6343d67e7e2e29d06a5a880db468c4a71587b60
goldwinalec <63246019+goldwinalec@users.noreply.github.com>
2021-01-27 15:24:52 +0500

    Update

--- js/script.js
+++ js/script.js
@@ -87,8 +87,6 @@ $(window).on('load resize', function() {
   if ($(window).width() < 769) {
     $('#slider:not(.slick-initialized)').slick({
       dots: true,
-      infinite: true,
-      speed: 2000,
       slidesToShow: 3,
       slidesToScroll: 3,
       arrows: false,
